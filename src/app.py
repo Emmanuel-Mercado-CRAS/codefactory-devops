9193148939b150a012a56aac0719df0f61ac4a74
Emmanuel-Mercado-CRAS <wellmil@hotmail.com>
2026-08-20 19:12:44 -0300

    fix: altera mensagem - Conflito A

---
+++ src/app.py
@@ -0,0 +1,11 @@
+# Aplicação principal - CodeFactory Solutions
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Conflito A - Mensagem do Dev 1"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
